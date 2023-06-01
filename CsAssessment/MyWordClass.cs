a29a7533d9c7c06780472bcaac16c95d07648544
Andy Wong <awong@entegral.com>
2023-06-01 08:16:31 -0700

    update

--- CsAssessment/MyWordClass.cs
+++ CsAssessment/MyWordClass.cs
@@ -25,22 +25,7 @@ namespace CsAssessment
               When input is null method should throw ArgumentNullException.
               The method should ignore all spaces at the beginning and at the end of the input text.
 
-        // Sample of a solution (to be removed before the candidate take the test)
-        public static string GetWordFromText(string? input, int wordNumberToFind)
-        {
-            // Todo: write code to implement the function as described above
-            if (wordNumberToFind < 1)
-                throw new ArgumentOutOfRangeException("number < than 1");
-            if (input == null)
-                throw new ArgumentNullException("input string is null");
-            input = input.Trim(); //removing spaces at beginning and end
-
-            var words = input.Split(' '); //won't work with multiple spaces
-            if (words.Length < wordNumberToFind)
-                throw new ArgumentOutOfRangeException("not enough words");
-            return words[wordNumberToFind - 1];
-        }
-
+        
         */
         /*
          * 2. Reverse 
@@ -57,22 +42,7 @@ namespace CsAssessment
            Method should throw ArgumentNullException if input is null
            Method should return empty string, if empty string is passed as an input parameter
         
-        // Sample of a solution (to be removed before the candidate take the test)
-        public static string Reverse(string input)
-        {
-            // Todo: write code to implement the function as described above
-            string result = String.Empty;
-            if (input == null)
-            {
-                throw new ArgumentNullException("string was null");
-            }
-            var inputChar = input.ToCharArray();
-            for (int i = input.Length - 1; i >= 0; i--)
-            {
-                result += inputChar[i];
-            }
-            return result;
-        }
+        
         */
 
         public static string GetWordFromText(string? input, int wordNumberToFind)
